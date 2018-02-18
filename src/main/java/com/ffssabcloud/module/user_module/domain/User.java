9d4565c66c83221b4f6ce0c6a035bcc4d7365ed3
FFSSabw <874566848@qq.com>
2018-02-18 21:01:39 +0800

    add entity User

---
+++ src/main/java/com/ffssabcloud/module/user_module/domain/User.java
@@ -0,0 +1,55 @@
+package com.ffssabcloud.module.user_module.domain;
+
+public class User {
+	private Integer id;
+	private String name;
+	private String email;
+	private String description;
+	private String location;
+	private String username;
+	@JsonIgnore
+	private String hashedPassword;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getHashedPassword() {
+		return hashedPassword;
+	}
+	public void setHashedPassword(String hashedPassword) {
+		this.hashedPassword = hashedPassword;
+	}
+}
